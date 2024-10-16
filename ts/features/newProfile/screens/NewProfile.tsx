cbe9c4a7af1076ef9cf05da3d53ac2f73d33cd90
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2024-10-16 10:35:20 +0200

    feat(NewProfile): delete switch

--- ts/features/newProfile/screens/NewProfile.tsx
+++ ts/features/newProfile/screens/NewProfile.tsx
@@ -1,11 +1,22 @@
-import { ContentWrapper, VSpacer } from "@pagopa/io-app-design-system";
+import {
+  ContentWrapper,
+  Divider,
+  ListItemSwitch,
+  VSpacer
+} from "@pagopa/io-app-design-system";
 import * as pot from "@pagopa/ts-commons/lib/pot";
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { UserDataProcessingChoiceEnum } from "../../../../definitions/backend/UserDataProcessingChoice";
 import { IOScrollViewWithLargeHeader } from "../../../components/ui/IOScrollViewWithLargeHeader";
 import I18n from "../../../i18n";
-import { loadUserDataProcessing } from "../../../store/actions/userDataProcessing";
+import { useIONavigation } from "../../../navigation/params/AppParamsList";
+import ROUTES from "../../../navigation/routes";
+import {
+  loadUserDataProcessing,
+  resetUserDataProcessingRequest
+} from "../../../store/actions/userDataProcessing";
 import { useIODispatch, useIOSelector } from "../../../store/hooks";
+import { userDataProcessingSelector } from "../../../store/reducers/userDataProcessing";
 import NewProfileContent from "../components/NewProfileContent";
 import NewProfileError from "../components/NewProfileError";
 import NewProfileLoading from "../components/NewProfileLoading";
@@ -15,6 +26,11 @@ import { selectNewProfile } from "../store/selectors";
 const NewProfileScreen = () => {
   const dispatch = useIODispatch();
   const newProfileSelector = useIOSelector(selectNewProfile);
+  const { DELETE } = useIOSelector(userDataProcessingSelector);
+  const navigation = useIONavigation();
+  const [isDeletionRequested, setIsDeletionRequested] = useState(
+    pot.isSome(DELETE)
+  );
 
   const loadProfile = useCallback(
     () => dispatch(newProfileActions.request()),
@@ -34,6 +50,10 @@ const NewProfileScreen = () => {
     checkUserDataProcessing();
   }, [checkUserDataProcessing, loadProfile]);
 
+  useEffect(() => {
+    setIsDeletionRequested(pot.isSome(DELETE));
+  }, [DELETE]);
+
   const NewProfileContentMapped = useCallback(
     () =>
       pot.fold(
@@ -50,6 +70,18 @@ const NewProfileScreen = () => {
     [loadProfile, newProfileSelector]
   );
 
+  const handleSwitchValueChange = useCallback(
+    (value: boolean) =>
+      value
+        ? navigation.navigate(ROUTES.NEW_PROFILE_STACK_NAVIGATOR, {
+            screen: ROUTES.NEW_PROFILE_DELETE_SCREEN
+          })
+        : dispatch(
+            resetUserDataProcessingRequest(UserDataProcessingChoiceEnum.DELETE)
+          ),
+    [dispatch, navigation]
+  );
+
   return (
     <IOScrollViewWithLargeHeader
       title={{
@@ -60,6 +92,15 @@ const NewProfileScreen = () => {
     >
       <ContentWrapper>
         <NewProfileContentMapped />
+        <Divider />
+        <ListItemSwitch
+          icon="info"
+          label="Stato cancellazione utenza"
+          value={isDeletionRequested}
+          description="Se attivato, la tua utenza sarà cancellata"
+          onSwitchValueChange={handleSwitchValueChange}
+          isLoading={pot.isLoading(DELETE)}
+        />
         <VSpacer size={48} />
       </ContentWrapper>
     </IOScrollViewWithLargeHeader>
